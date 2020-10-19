5ed031b865749b2ded82532000fc13465a0d4234
wenhui_xiao <wenhui_xiao@9splay.com>
2020-10-19 15:32:37 +0800

    更新

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -13,13 +13,22 @@ namespace RegexTest
         public ListNode next;
         public ListNode(int x) { val = x; }
     }
-
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
     class Program
     {
-        static int[] TestArray = { 12, 345, 2, 6, 7896 };
+        static int[] TestArray = { 0,1,2,3,4};
+        static int[] TestArray2 = { 0,1,2,2,1};
         static int TestNumber = 234;
-        static string TestStr = "RLRRRLLRLL";
+        static string TestStr1 = "aA";
+        static string TestStr2 = "aAAbbbb";
         static ListNode TestNode = GetTestNode();
+        static TreeNode TestTreeNode = GetTestTreeNode();
         static public ListNode GetTestNode()
         {
             ListNode node = new ListNode(1);
@@ -28,27 +37,51 @@ namespace RegexTest
             node.next = node2;
             return node;
         }
+        static public TreeNode GetTestTreeNode()
+        {
+            TreeNode node = new TreeNode(10);
+            node.left = new TreeNode(5);
+            node.right = new TreeNode(15);
+            node.left.left = new TreeNode(3);
+            node.left.right = new TreeNode(7);
+            node.right.right = new TreeNode(18);
+            return node;
+        }
+        static public string ArrayAnstoString(int[] ans)
+        {
+            string str= "[";
+            for(int i = 0; i < ans.Length; i++)
+            {
+                str += ans[i] + ",";
+            }
+            str = str.Substring(0,str.Length-1)+"]";
+            return str;
+        }
         static void Main(string[] args)
         {
-            int ans = GetDecimalValue(TestNode);
-            Console.WriteLine("ans = " + ans);
+            //int ans = RangeSumBST(TestTreeNode, 7, 15 );
+            //Console.WriteLine("ans = " + ans);
+            int[] ans = CreateTargetArray(TestArray, TestArray2);
+            Console.WriteLine(ArrayAnstoString(ans));
             Console.ReadLine();
         }
-        static public int GetDecimalValue(ListNode head)
+        static public int[] CreateTargetArray(int[] nums, int[] index)
         {
-            List<int> list = new List<int>();
-            int ans = 0;
-            while (true)
+            int?[] tmp = new int?[nums.Length];
+            int[] ans = new int[nums.Length];
+            int cnt;
+            for(int i = nums.Length-1; i >= 0; i--)
             {
-
-                list.Add(head.val);
-                if (head.next == null)
-                    break;
-                head = head.next;
+                cnt = i;
+                while (tmp[cnt] !=null)
+                {
+                    cnt++;
+                }
+                tmp[index[i]] = nums[cnt];
             }
-            for(int i = 0; i < list.Count; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                ans += Convert.ToInt32(list[list.Count-i-1] *Math.Pow(2,i));
+                ans[i] = tmp[i].GetValueOrDefault();
             }
             return ans;
         }
